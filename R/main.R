489f61216a6fa5ea5bb6ba3d3b4ec8564a1cf01a
paul-antoine <paul-antoine.chevalier@data.gouv.fr>
2016-08-04 04:53:02 -0400

    delete old files

--- R/main.R
+++
@@ -1,8 +0,0 @@
-
-library(readr)
-
-read_csv("data/table_departements.csv") %>% glimpse()
-read_csv("data/table_regions_2016.csv") %>% glimpse()
-read_csv("data/table_defm_departement_categorie.csv") %>% glimpse()
-read_csv("data/table_defm_region2016_categorie.csv") %>% glimpse()
-read_csv("data/table_defm_france_categorie.csv") %>% glimpse()
